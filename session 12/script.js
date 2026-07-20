28c310e37698e235af8b24ff1821a3759492111a
Amany <amanysalah5552@gmail.com>
2026-07-20 12:08:51 +0300

    session 12

---
+++ session 12/script.js
@@ -0,0 +1,13 @@
+let courses = ["html", "css", "js"]
+
+let input = prompt("Enter your course")
+
+let findIndex = courses.findIndex((item) => item == input)
+
+if (findIndex !== -1) {
+  alert("The index of the course is " + findIndex)
+} else {
+  courses.push(input)
+  let newIndex = courses.length - 1
+  alert("The course is not found and added to the index " + newIndex)
+}
